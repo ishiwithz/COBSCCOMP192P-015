e42550fa3008982187bf75229f51d969d427fef5
Ishanka Withanage <44730029+ishiwithz@users.noreply.github.com>
2021-01-30 19:00:21 +0530

    Add files via upload

---
+++ Plex/Plex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Plex
+//
+//  Created by Ishanka Withanage on 2021-01-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
